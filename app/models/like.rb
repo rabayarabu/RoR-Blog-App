3c1692917dded01da9f82d167ccdbcf79ea8a99a
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-09-29 19:30:46 +0600

    Update likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,15 @@
 class Like < ApplicationRecord
   belongs_to :post, class_name: 'Post'
   belongs_to :user, class_name: 'User'
+  # Method to update the likes counter for a post
+  after_create :increment_post_likes_counter
+  after_destroy :decrement_post_likes_counter
+
+  def increment_post_likes_counter
+    post.increment!(:likes_count)
+  end
+
+  def decrement_post_likes_counter
+    post.decrement!(:likes_count)
+  end
 end
