b200590ccd5c5a3c8e56e5ff82c2f782f62a3896
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-09-29 19:29:00 +0600

    Add recent-comments methods for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,20 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
+  # Method to update the posts counter for a user
+  after_create :increment_user_posts_counter
+  after_destroy :decrement_user_posts_counter
+
+  def increment_user_posts_counter
+    author.increment!(:posts_count)
+  end
+
+  def decrement_user_posts_counter
+    author.decrement!(:posts_count)
+  end
+
+  # Method to return the 5 most recent comments for a given post
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
