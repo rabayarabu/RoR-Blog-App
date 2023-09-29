5701a14efeea4d7cd20b043dfeaf9eb668ccfb08
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-09-29 19:30:08 +0600

    Update comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+  # Method to update the comments counter for a post
+  after_create :increment_post_comments_counter
+  after_destroy :decrement_post_comments_counter
+
+  def increment_post_comments_counter
+    post.increment!(:comments_count)
+  end
+
+  def decrement_post_comments_counter
+    post.decrement!(:comments_count)
+  end
 end
