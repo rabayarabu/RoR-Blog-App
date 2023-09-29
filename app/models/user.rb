00420320d92b97b95a33a5d3ddfdea53a9137cf9
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-09-29 19:27:28 +0600

    Created recent-posts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
+  # Method to return the 3 most recent posts for a given user
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
 end
