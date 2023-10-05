eb831dcaffc92bf1eebb0b40aa6381ee9df09589
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-10-05 20:21:56 +0600

    Create new post

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,20 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
-  # Users routes
   get "/users" , to: "users#index",  as: "users"
   get "/users/:id" , to: "users#show" , as: 'user'
-
-  # Posts routes
   get "/users/:id/posts" , to: "posts#index", as: "user_posts"
   get "/users/:id/posts/:id" , to: "posts#show", as: "user_post"
+  get "/users/:id/posts/new" , to: "posts#new"
+  # resources :users, only: [:index, :show] do
+  #   resources :posts, only: [:index, :show, :new, :create] do
+  #     resources :comments, only: [:new, :create]
+  #     resources :likes, only: [:create]
+  #   end
+  # end
+  resources :users do
+    resources :posts do
+      resources :comments
+    end
+  end
 end
